fe935c368418ec9c212d9e4ad852d7c3b47833fe
Dhara Patel <dhara.patel@letsnurture.com>
2018-06-06 12:26:51 +0530

    Project added

---
+++ CI_Demo/CI_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CI_Demo
+//
+//  Created by LN-MCMI-006 on 06/06/18.
+//  Copyright © 2018 LN-MCMI-006. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
